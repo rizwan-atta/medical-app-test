9174dcd81166418788aaecdf526fe13165b5c053
rizwan-atta <rizwancontact@yahoo.com>
2024-02-08 12:59:33 -0500

    included landing page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/Landing_Page';
+
 function App() {
 
   return (
     <div className="App">
         <BrowserRouter>
           <Navbar/>
+          
               <Routes>
+              <Route path='/' element={<LandingPage/>} />
               </Routes>
             
         </BrowserRouter>
