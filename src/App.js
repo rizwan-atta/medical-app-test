6f49f9ace3a6e47ceff9496c6d1b0dcb5afac1fd
rizwan-atta <rizwancontact@yahoo.com>
2024-02-10 03:35:19 -0500

    till landing signup login  page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Navbar from './Components/Navbar/Navbar';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
+
 function App() {
 
   return (
@@ -11,6 +14,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Sign_Up" element={<Sign_Up />} />
+              <Route path="/Login" element={<Login />} />
               </Routes>
             
         </BrowserRouter>
