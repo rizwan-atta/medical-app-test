d80383f67f3d1b97aacc0f2ab4e8766ed5e12f25
rizwan-atta <rizwancontact@yahoo.com>
2024-02-10 06:35:55 -0500

    till doctor serach

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
               <Route path="/Sign_Up" element={<Sign_Up />} />
               <Route path="/Login" element={<Login />} />
               <Route path="/instant-consultation" element={<InstantConsultation />} />
+              <Route path='/finddoctor' element={<FindDoctorSearch />} />
               </Routes>
             
         </BrowserRouter>
