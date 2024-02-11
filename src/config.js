38691b82a84a2d33b5151cc8b1d23a9208c0cef3
rizwan-atta <rizwancontact@yahoo.com>
2024-02-11 00:31:58 -0500

    seo

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+export const API_URL = window.location.origin 
+//export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
 console.log(
     "API_URL :",
     API_URL
